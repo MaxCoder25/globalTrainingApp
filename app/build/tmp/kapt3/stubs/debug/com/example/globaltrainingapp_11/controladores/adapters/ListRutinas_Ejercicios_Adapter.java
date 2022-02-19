aa8b1239af6defbf1b15cdfa80506f5847e815c5
MaxCoder25 <98476953+MaxCoder25@users.noreply.github.com>
2022-02-19 11:23:19 -0500

    Tablas rutinas, ejercicios y relacion many to many agregadas y funcionando, activity ejercicios y activity entrenamiento (dentro de rutinas) recicler view desde base y muestra imagenes de internet, agregadas rutinas de ejemplo para todas las categorias

--- app/build/tmp/kapt3/stubs/debug/com/example/globaltrainingapp_11/controladores/adapters/ListRutinas_Ejercicios_Adapter.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/globaltrainingapp_11/controladores/adapters/ListRutinas_Ejercicios_Adapter.java
@@ -2,20 +2,20 @@ package com.example.globaltrainingapp_11.controladores.adapters;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u00004\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0010\u0002\n\u0002\b\u0004\n\u0002\u0010\b\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\b\u0012\u0004\u0012\u00020\u00020\u0001B\'\u0012\f\u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004\u0012\u0012\u0010\u0006\u001a\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\b0\u0007\u00a2\u0006\u0002\u0010\tJ\b\u0010\f\u001a\u00020\rH\u0016J\u0018\u0010\u000e\u001a\u00020\b2\u0006\u0010\u000f\u001a\u00020\u00022\u0006\u0010\u0010\u001a\u00020\rH\u0016J\u0018\u0010\u0011\u001a\u00020\u00022\u0006\u0010\u0012\u001a\u00020\u00132\u0006\u0010\u0014\u001a\u00020\rH\u0016R\u0017\u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\n\u0010\u000bR\u001a\u0010\u0006\u001a\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\b0\u0007X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0015"}, d2 = {"Lcom/example/globaltrainingapp_11/controladores/adapters/ListRutinas_Ejercicios_Adapter;", "Landroidx/recyclerview/widget/RecyclerView$Adapter;", "Lcom/example/globaltrainingapp_11/controladores/adapters/Rutinas_EjerciciosViewHolder;", "listRutinas_Ejercicios", "", "Lcom/example/globaltrainingapp_11/entidades/Rutinas_Ejercicios_Entity;", "onClickItemSelected", "Lkotlin/Function1;", "", "(Ljava/util/List;Lkotlin/jvm/functions/Function1;)V", "getListRutinas_Ejercicios", "()Ljava/util/List;", "getItemCount", "", "onBindViewHolder", "holder", "position", "onCreateViewHolder", "parent", "Landroid/view/ViewGroup;", "viewType", "app_debug"})
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u00004\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0010\u0002\n\u0002\b\u0004\n\u0002\u0010\b\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\b\u0012\u0004\u0012\u00020\u00020\u0001B\'\u0012\f\u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004\u0012\u0012\u0010\u0006\u001a\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\b0\u0007\u00a2\u0006\u0002\u0010\tJ\b\u0010\f\u001a\u00020\rH\u0016J\u0018\u0010\u000e\u001a\u00020\b2\u0006\u0010\u000f\u001a\u00020\u00022\u0006\u0010\u0010\u001a\u00020\rH\u0016J\u0018\u0010\u0011\u001a\u00020\u00022\u0006\u0010\u0012\u001a\u00020\u00132\u0006\u0010\u0014\u001a\u00020\rH\u0016R\u0017\u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\n\u0010\u000bR\u001a\u0010\u0006\u001a\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\b0\u0007X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0015"}, d2 = {"Lcom/example/globaltrainingapp_11/controladores/adapters/ListRutinas_Ejercicios_Adapter;", "Landroidx/recyclerview/widget/RecyclerView$Adapter;", "Lcom/example/globaltrainingapp_11/controladores/adapters/Rutinas_EjerciciosViewHolder;", "listRutinas_Ejercicios", "", "Lcom/example/globaltrainingapp_11/entidades/EjerciciosEntity;", "onClickItemSelected", "Lkotlin/Function1;", "", "(Ljava/util/List;Lkotlin/jvm/functions/Function1;)V", "getListRutinas_Ejercicios", "()Ljava/util/List;", "getItemCount", "", "onBindViewHolder", "holder", "position", "onCreateViewHolder", "parent", "Landroid/view/ViewGroup;", "viewType", "app_debug"})
 public final class ListRutinas_Ejercicios_Adapter extends androidx.recyclerview.widget.RecyclerView.Adapter<com.example.globaltrainingapp_11.controladores.adapters.Rutinas_EjerciciosViewHolder> {
     @org.jetbrains.annotations.NotNull()
-    private final java.util.List<com.example.globaltrainingapp_11.entidades.Rutinas_Ejercicios_Entity> listRutinas_Ejercicios = null;
-    private final kotlin.jvm.functions.Function1<com.example.globaltrainingapp_11.entidades.Rutinas_Ejercicios_Entity, kotlin.Unit> onClickItemSelected = null;
+    private final java.util.List<com.example.globaltrainingapp_11.entidades.EjerciciosEntity> listRutinas_Ejercicios = null;
+    private final kotlin.jvm.functions.Function1<com.example.globaltrainingapp_11.entidades.EjerciciosEntity, kotlin.Unit> onClickItemSelected = null;
     
     public ListRutinas_Ejercicios_Adapter(@org.jetbrains.annotations.NotNull()
-    java.util.List<com.example.globaltrainingapp_11.entidades.Rutinas_Ejercicios_Entity> listRutinas_Ejercicios, @org.jetbrains.annotations.NotNull()
-    kotlin.jvm.functions.Function1<? super com.example.globaltrainingapp_11.entidades.Rutinas_Ejercicios_Entity, kotlin.Unit> onClickItemSelected) {
+    java.util.List<com.example.globaltrainingapp_11.entidades.EjerciciosEntity> listRutinas_Ejercicios, @org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function1<? super com.example.globaltrainingapp_11.entidades.EjerciciosEntity, kotlin.Unit> onClickItemSelected) {
         super();
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final java.util.List<com.example.globaltrainingapp_11.entidades.Rutinas_Ejercicios_Entity> getListRutinas_Ejercicios() {
+    public final java.util.List<com.example.globaltrainingapp_11.entidades.EjerciciosEntity> getListRutinas_Ejercicios() {
         return null;
     }
     
---
+++ app/build/tmp/kapt3/stubs/debug/com/example/globaltrainingapp_11/data/database/dao/EjerciciosDAO.java
@@ -0,0 +1,68 @@
+package com.example.globaltrainingapp_11.data.database.dao;
+
+import java.lang.System;
+
+@androidx.room.Dao()
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010 \n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0002\b\u0005\bg\u0018\u00002\u00020\u0001J\u0011\u0010\u0002\u001a\u00020\u0003H\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0004J\u0019\u0010\u0005\u001a\u00020\u00032\u0006\u0010\u0006\u001a\u00020\u0007H\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\bJ\u0019\u0010\t\u001a\u00020\u00032\u0006\u0010\n\u001a\u00020\u000bH\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\fJ\u0017\u0010\r\u001a\b\u0012\u0004\u0012\u00020\u000b0\u000eH\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0004J\u0019\u0010\u000f\u001a\u00020\u000b2\u0006\u0010\u0010\u001a\u00020\u0011H\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0012J\u0019\u0010\u0013\u001a\u00020\u000b2\u0006\u0010\u0014\u001a\u00020\u0007H\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\bJ\u0019\u0010\u0015\u001a\u00020\u000b2\u0006\u0010\u0016\u001a\u00020\u0011H\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0012J\u000e\u0010\u0017\u001a\b\u0012\u0004\u0012\u00020\u00180\u000eH\'J\u0016\u0010\u0019\u001a\b\u0012\u0004\u0012\u00020\u00180\u000e2\u0006\u0010\u001a\u001a\u00020\u0007H\'J\u0019\u0010\u001b\u001a\u00020\u00032\u0006\u0010\n\u001a\u00020\u000bH\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\fJ\u0019\u0010\u001c\u001a\u00020\u00032\u0006\u0010\n\u001a\u00020\u000bH\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\f\u0082\u0002\u0004\n\u0002\b\u0019\u00a8\u0006\u001d"}, d2 = {"Lcom/example/globaltrainingapp_11/data/database/dao/EjerciciosDAO;", "", "cleanDbEjercicios", "", "(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "deleteEjerciciosById", "idEjercicios", "", "(ILkotlin/coroutines/Continuation;)Ljava/lang/Object;", "deleteOneEjercicios", "ejercicio", "Lcom/example/globaltrainingapp_11/entidades/EjerciciosEntity;", "(Lcom/example/globaltrainingapp_11/entidades/EjerciciosEntity;Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "getAllEjercicios", "", "getEjerciciosByCategory", "EjerciciosCategory", "", "(Ljava/lang/String;Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "getEjerciciosById", "idEjercicio", "getEjerciciosByTipoMovimiento", "EjerciciosTipoMovimiento", "getRutinas_Ejercicios_Relaciones", "Lcom/example/globaltrainingapp_11/entidades/Rutinas_Ejercicios_Relaciones;", "getRutinas_Ejercicios_RelacionesFiltradas", "id_rutinas", "insertEjercicios", "updateEjercicios", "app_debug"})
+public abstract interface EjerciciosDAO {
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Query(value = "SELECT * FROM ejercicios")
+    public abstract java.lang.Object getAllEjercicios(@org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super java.util.List<com.example.globaltrainingapp_11.entidades.EjerciciosEntity>> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Query(value = "SELECT * FROM ejercicios WHERE id_ejercicios = :idEjercicio")
+    public abstract java.lang.Object getEjerciciosById(int idEjercicio, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super com.example.globaltrainingapp_11.entidades.EjerciciosEntity> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Query(value = "SELECT * FROM ejercicios WHERE categoria = :EjerciciosCategory")
+    public abstract java.lang.Object getEjerciciosByCategory(@org.jetbrains.annotations.NotNull()
+    java.lang.String EjerciciosCategory, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super com.example.globaltrainingapp_11.entidades.EjerciciosEntity> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Query(value = "SELECT * FROM ejercicios WHERE tipo_movimiento = :EjerciciosTipoMovimiento")
+    public abstract java.lang.Object getEjerciciosByTipoMovimiento(@org.jetbrains.annotations.NotNull()
+    java.lang.String EjerciciosTipoMovimiento, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super com.example.globaltrainingapp_11.entidades.EjerciciosEntity> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Insert(onConflict = androidx.room.OnConflictStrategy.REPLACE)
+    public abstract java.lang.Object insertEjercicios(@org.jetbrains.annotations.NotNull()
+    com.example.globaltrainingapp_11.entidades.EjerciciosEntity ejercicio, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super kotlin.Unit> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Update(onConflict = androidx.room.OnConflictStrategy.REPLACE)
+    public abstract java.lang.Object updateEjercicios(@org.jetbrains.annotations.NotNull()
+    com.example.globaltrainingapp_11.entidades.EjerciciosEntity ejercicio, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super kotlin.Unit> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Delete()
+    public abstract java.lang.Object deleteOneEjercicios(@org.jetbrains.annotations.NotNull()
+    com.example.globaltrainingapp_11.entidades.EjerciciosEntity ejercicio, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super kotlin.Unit> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Query(value = "DELETE FROM ejercicios")
+    public abstract java.lang.Object cleanDbEjercicios(@org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super kotlin.Unit> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Query(value = "DELETE FROM ejercicios WHERE id_ejercicios = :idEjercicios")
+    public abstract java.lang.Object deleteEjerciciosById(int idEjercicios, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super kotlin.Unit> continuation);
+    
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.Query(value = "SELECT * FROM rutinas")
+    @androidx.room.Transaction()
+    public abstract java.util.List<com.example.globaltrainingapp_11.entidades.Rutinas_Ejercicios_Relaciones> getRutinas_Ejercicios_Relaciones();
+    
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.Query(value = "SELECT * FROM rutinas  WHERE id_rutinas=:id_rutinas")
+    @androidx.room.Transaction()
+    public abstract java.util.List<com.example.globaltrainingapp_11.entidades.Rutinas_Ejercicios_Relaciones> getRutinas_Ejercicios_RelacionesFiltradas(int id_rutinas);
+}
