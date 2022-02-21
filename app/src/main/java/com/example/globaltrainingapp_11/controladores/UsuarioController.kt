2b57aef96807fbac98f87b9c7d21ce8154fef58a
MaxCoder25 <98476953+MaxCoder25@users.noreply.github.com>
2022-02-21 16:58:04 -0500

    Update UsuarioController.kt

--- app/src/main/java/com/example/globaltrainingapp_11/controladores/UsuarioController.kt
+++ app/src/main/java/com/example/globaltrainingapp_11/controladores/UsuarioController.kt
@@ -4,11 +4,12 @@ import com.example.globaltrainingapp_11.logica.UsuarioBL
 
 class UsuarioController {
 
-    fun LoginUser(name: String, pass: String): Boolean {
+  /*  fun LoginUser(name: String, pass: String): Boolean {
         return UsuarioBL().LoginUser(
             name.trim().uppercase(),
             pass.trim().lowercase()
         )
     }
+    */
 
 }
